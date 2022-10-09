7ce28f5929597a12bbc9b04dcf85b7b3c5c735d0
MerJP <merjuradoperez@gmail.com>
2022-10-09 06:34:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+const app = express()
+require('dotenv').config()
+require('./config/db.config')
+
+
+app.use(cors())
+app.use(express.json())
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`server started on port ${process.env.PORT}`);
+})
